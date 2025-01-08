51b4c9703af2b31817b68243714ff20cb8375fc7
Jörn Menne <jmenne@fedora.de>
2025-01-08 12:09:47 +0100

    Add a license
    The Gplv3.0 is used

--- georeport/__init__.py
+++ georeport/__init__.py
@@ -1,1 +1,3 @@
+# Copyright: (c) 2025, Jörn Menne <jmenne@posteo.de>
+# GNU General Public License v3.0 (see LICSENE or https://www.gnu.org/license/gpl-3.0.md)
 __version__ = "0.1.0"
--- georeport/admin.py
+++ georeport/admin.py
@@ -1,3 +1,7 @@
+# Copyright: (c) 2025, Jörn Menne <jmenne@posteo.de>
+# GNU General Public License v3.0 (see LICSENE or https://www.gnu.org/license/gpl-3.0.md)
+
+
 from django.contrib import admin
 
 # Register your models here.
