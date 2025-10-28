828e634c6d286d6d1a866e9f013720ef63f2aa2d
Jörn Menne <jmenne@localhost.de>
2025-10-28 21:52:48 +0100

    Revert "Deactivate login-page to use admin"
    This reverts commit cba4eac93ba486e7e6241bc67d15617f8b847649.

--- geoentries/urls.py
+++ geoentries/urls.py
@@ -2,6 +2,7 @@
 # Licensed under the Apache License, Version 2.0
 # See NOTICE file for details.
 
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -22,22 +23,19 @@ urlpatterns = [
     path("create", views.EntryCreateView.as_view(), name="create"),
     path("entries", views.EntryListView.as_view(), name="list"),
     path("entry/<int:pk>", views.EntryDetailView.as_view(), name="detail"),
-    # path("update/<int:pk>", views.EntryUpdateView.as_view(), name="update"),
+    path("update/<int:pk>", views.EntryUpdateView.as_view(), name="update"),
     path("locations", views.get_location_data, name="locations"),
     path("open311/v2/", include(router.urls)),
     path("<str:b64nonce>/<str:b64ct>", views.close_with_link_view, name="finish"),
 ]
 
-# NOTE: Login, Logout and update are commented out in order to use the django-admin
-# as the main portal to edit entries
-
-# urlpatterns += [
-#    path(
-#        "login", auth_views.LoginView.as_view(next_page="geoentries:list"), name="login"
-#    ),
-#    path(
-#        "logout",
-#        auth_views.LogoutView.as_view(next_page="geoentries:index"),
-#        name="logout",
-#    ),
-# ]
+urlpatterns += [
+    path(
+        "login", auth_views.LoginView.as_view(next_page="geoentries:list"), name="login"
+    ),
+    path(
+        "logout",
+        auth_views.LogoutView.as_view(next_page="geoentries:index"),
+        name="logout",
+    ),
+]
